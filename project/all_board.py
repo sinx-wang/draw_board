acd80cfb5fd37eed69652c77203f72ebff5e5b09
sinx-wang <cosx1999@hotmail.com>
2018-09-25 20:08:25 +0800

    基本完成

---
+++ project/all_board.py
@@ -0,0 +1,248 @@
+import os
+import pygame
+from pygame.locals import *
+import shape_analysis
+
+class __button__():
+    def __init__(self, image, position, screen):
+        self.image = pygame.image.load(image).convert_alpha()
+        self.position = position
+        self.screen = screen
+
+    # 判断鼠标是否在按钮上
+    def isOver(self):
+        point_x, point_y = pygame.mouse.get_pos()
+        x, y = self.position
+        w, h = self.image.get_size()
+
+        in_x = x - w/2 < point_x < x + w/2
+        in_y = y - h/2 < point_y < y + h/2
+        return in_x and in_y
+
+    def render(self):
+        x, y = self.position
+        w, h = self.image.get_size()
+        self.screen.blit(self.image, (x - w / 2, y - h / 2))
+
+class Brush():
+    def __init__(self, screen):
+        self.screen = screen
+        self.color = (0, 0, 0)
+        self.size = 1
+        self.drawing = False
+        self.last_pos = None
+
+    def start_draw(self, pos):
+        self.drawing = True
+        self.last_pos = pos
+
+    def end_draw(self):
+        self.drawing = False
+
+    def draw(self, pos):
+        if self.drawing:
+            # pygame.draw.circle(self.screen, self.color, pos, self.size)
+            pygame.draw.line(self.screen, self.color, self.last_pos, pos, self.size * 2)
+            self.last_pos = pos
+
+class Text():
+    def __init__(self, position, content):
+        self.content = content
+        self.position = position
+
+    def display_text(self, screen):
+        pygame.font.init()
+        self.my_font = pygame.font.SysFont("arial", 16)
+        self.text_surface = self.my_font.render(self.content, True, (0, 0, 0))
+        x, y = self.position
+        screen.blit(self.text_surface, (x, y))
+
+class Painter():
+    def __init__(self):
+        self.make_dirs()
+        white_screen_width = 900
+        white_screen_height = 650
+        self.white_surface = pygame.Surface([white_screen_width, white_screen_height])
+
+        button_screen_width = 900
+        button_screen_height = 50
+        self.button_surface = pygame.Surface([button_screen_width, button_screen_height])
+
+        self.screen = pygame.display.set_mode((900, 650))
+        pygame.display.set_caption("Drawboard")
+        self.clock = pygame.time.Clock()
+
+        self.text_list = []# 现在这一张图里的标注信息
+        self.current_surface_no = -1
+        # 是否到了两侧的界限
+        self.left_stop = True
+        self.right_stop = True
+
+    def make_dirs(self):
+        if not os.path.exists("Pictures"):
+            os.mkdir("Pictures")
+        if not os.path.exists("Infomation"):
+            os.mkdir("Infomation")
+
+    def find_pic_num(self):
+        pic_dir_path = "Pictures"
+        max_num = 0
+        max_num = len([name for name in os.listdir(pic_dir_path) if os.path.isfile(os.path.join(pic_dir_path, name))])
+        return max_num
+
+
+    def recognize(self, surface):
+        # 保存图片
+        self.current_surface_no = -1
+        pic_dir_path = "Pictures"
+        info_dir_path = "Infomation"
+        max_num = self.find_pic_num()
+        pic_path = os.path.join(pic_dir_path, str(max_num) + ".png")
+        info_path = os.path.join(info_dir_path, str(max_num) + ".txt")
+        pygame.image.save(surface, pic_path)
+        info_file = open(info_path, 'a')
+
+        # 识别图片
+        ld = shape_analysis.shape_analysis(pic_path)
+        analysis_result = ld.analysis()
+        for shape_data in analysis_result:
+            info_file.write(shape_data.shape_type + " " + str(shape_data.list[0]) + "," +str(shape_data.list[1]))
+            info_file.write('\n')
+
+        info_file.close()
+        self.load_info(info_path)
+
+    def load_info(self, path):
+        # 把读到的标注信息放入text_list中
+        input = open(path)
+        for line in input:
+            type_str, pos_str = line.split()
+            pos = [int(pos_str.split(',')[0]), int(pos_str.split(',')[1])]
+            text = Text(pos, type_str)
+            self.text_list.append(text)
+        input.close()
+
+    def load_history_surface(self):
+        num = self.current_surface_no # -1代表画板
+
+        if num < 0:
+            if self.find_pic_num() == 0:
+                self.left_stop = True
+            else:
+                self.left_stop = False
+            self.right_stop = True
+            self.white_surface.fill((255, 255, 255))
+            self.text_list = []
+        else: # 读过去的
+            if num == 0:
+                self.left_stop = True
+            else:
+                self.left_stop = False
+            self.right_stop = False
+            pic_path = "Pictures/" + str(num) + ".png"
+            image = pygame.image.load(pic_path).convert_alpha()
+            self.white_surface.blit(image, (0, 0))
+
+            info_path = "Infomation/" + str(num) + ".txt"
+            self.load_info(info_path)
+
+    def button_view(self):
+        stop_button_path = 'Resourse/button_stop.png'
+        self.recongnize_button = __button__('Resourse/button_confirm.png', (100, 625), self.screen)
+        self.refresh_button = __button__('Resourse/button_refresh.png', (200, 625), self.screen)
+        if self.left_stop:
+            left_button_path = stop_button_path
+        else:
+            left_button_path = 'Resourse/button_left.png'
+        if self.right_stop:
+            right_button_path = stop_button_path
+        else:
+            right_button_path = 'Resourse/button_right.png'
+        self.left_button = __button__(left_button_path, (650, 625), self.screen)
+        self.right_button = __button__(right_button_path, (750, 625), self.screen)
+        self.recongnize_button.render()
+        self.refresh_button.render()
+        self.left_button.render()
+        self.right_button.render()
+
+    def show_surface(self, isLeft):
+        sum = self.find_pic_num()
+        num = self.current_surface_no
+        if sum == 0:
+            self.load_history_surface()
+            return
+        if isLeft:
+            if num == -1: # 最新编辑状态
+                self.current_surface_no = sum - 1
+                self.text_list = []
+            elif num == 0:
+                return
+            else:
+                self.current_surface_no -= 1
+                self.text_list = []
+        else:
+            if num == -1:
+                return
+            self.text_list = []
+            if num == sum - 1:
+                self.current_surface_no = -1
+            else:
+                self.current_surface_no += 1
+
+        self.load_history_surface()
+
+    def run(self):
+        self.white_surface.fill((255, 255, 255))
+
+        self.current_surface_no = -1
+        self.load_history_surface()
+
+        self.button_surface.fill((0, 255, 255))
+        self.brush = Brush(self.white_surface)
+
+        while True:
+            self.clock.tick(30)
+
+            self.screen.blit(self.white_surface, (0, 0))
+            self.screen.blit(self.button_surface, (0, 600))
+
+            self.button_view()
+
+            if len(self.text_list) > 0:
+                for word in self.text_list:
+                    t = Text(word.position, word.content)
+                    t.display_text(self.screen)
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        self.white_surface.fill((255, 255, 255))
+                        self.text_list = []
+                    if event.key == K_RETURN: # 回车事件
+                        self.recognize(self.white_surface)
+                    if event.key == K_LEFT:
+                        self.show_surface(isLeft = True)
+                    if event.key == K_RIGHT:
+                        self.show_surface(isLeft = False)
+                elif event.type == MOUSEBUTTONDOWN:
+                    if self.recongnize_button.isOver():
+                        self.recognize(self.white_surface)
+                    if self.refresh_button.isOver():
+                        self.white_surface.fill((255, 255, 255))
+                        self.text_list = []
+                    if self.left_button.isOver():
+                        self.show_surface(isLeft=True)
+                    if self.right_button.isOver():
+                        self.show_surface(isLeft=False)
+                    self.brush.start_draw(pygame.mouse.get_pos())
+                elif event.type == MOUSEMOTION:
+                    self.brush.draw(event.pos)
+                elif event.type == MOUSEBUTTONUP:
+                    self.brush.end_draw()
+            pygame.display.update()
+
+if __name__ == '__main__':
+    app = Painter()
+    app.run()
---
+++ project/shape_analysis.py
@@ -0,0 +1,72 @@
+import cv2 as cv
+import numpy as np
+
+# 数据结构，String shape_type, [x, y]
+class shape_data():
+    def __init__(self, shape_type, list):
+        self.shape_type = shape_type
+        self.list = list
+
+class shape_analysis():
+    def __init__(self, filename):
+        self.src = cv.imread(filename)
+        self.shape_list = []
+
+    def analysis(self):
+        h, w, ch = self.src.shape
+        result = np.zeros((h, w, ch), dtype=np.uint8)
+        # 二值化图像
+        print("start to detect lines……")
+        gray = cv.cvtColor(self.src, cv.COLOR_BGR2GRAY)
+        ret, binary = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)
+
+        out_binary, contours, hierarchy = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        for cnt in range(len(contours)):
+
+            # 轮廓逼近
+            par = 0.01
+            i = 1
+            while i >= 0:
+                epsilon = par * cv.arcLength(contours[cnt], True)
+                approx = cv.approxPolyDP(contours[cnt], epsilon, True)
+
+                # 分析几何形状
+                corners = len(approx)
+                shape_type = ""
+                print(corners)
+                if corners == 3:
+                    shape_type = "triangle"
+                    break
+                elif corners == 4:
+                    shape_type = "rectangle"
+                    break
+                elif corners >= 10:
+                    shape_type = "circle"
+                    break
+                elif 4 < corners < 10:
+                    shape_type = "polygon"
+                    par = 0.10
+                else:
+                    shape_type = "others"
+                    par = 0.01
+
+                i -= 1
+
+            # 求解中心位置
+            mm = cv.moments(contours[cnt])
+            cx = int(mm['m10'] / mm['m00'])
+            cy = int(mm['m01'] / mm['m00'])
+            # cv.circle(result, (cx, cy), 3, (0, 0, 255), -1)
+            print(cx, cy)
+
+            print("形状: %s" % (shape_type))
+
+            shape = shape_data(shape_type, [cx, cy])
+            self.shape_list.append(shape)
+
+        return self.shape_list
+
+if __name__ == "__main__":
+    ld = shape_analysis("Pictures/7.png")
+    print(ld.analysis())
+
